6823f0f8287623a7e617036cc4d4bc5cc5569cd0
LSC <lelandscrawford@gmail.com>
2021-01-17 23:26:59 -0500

    adding

--- FCCPython.py
+++ FCCPython.py
@@ -1,4 +1,4 @@
-x = input("Give me a word:")
+x = input("Give me a word: ")
 
 def flip(x):
     return x[::-1]
@@ -8,11 +8,11 @@ print(flip(x))
 def rev(y):
     z = []
 
-    for i in range(len(x), 0, -1):
+    for i in range(len(y), 0, -1):
         z.append(y(i))
     return z
 
-y = input("Give me another word")
+y = input("Give me another word: ")
 
 print(rev(y))
 
